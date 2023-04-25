d73a603100ad32e0770011944832c6959752c68e
prajwallaK <prajwalla143@gmail.com>
2023-04-25 19:25:36 -0400

    first commit

---
+++ Sensei.java
@@ -0,0 +1,11 @@
+package com;
+
+public class Sensei {
+
+	public static void main(String[] args) {
+		System.out.println("helloJava");
+	    System.out.println("this line is second comit");
+
+	}
+
+}
